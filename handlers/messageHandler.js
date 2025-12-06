936bfe5750f233d1ba3ebbdde368bb6d6557000d
juanfu7467v <movistarcervise@gmail.com>
2025-12-06 15:08:32 -0500

    Create messageHandler.js

---
+++ handlers/messageHandler.js
@@ -0,0 +1,9 @@
+import { workerData, parentPort } from "worker_threads";
+
+function processMessage({ message }) {
+  return `Hola 👋 recibí tu mensaje: ${message}`;
+}
+
+const response = processMessage(workerData);
+
+parentPort.postMessage(response);
