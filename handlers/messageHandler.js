e6b0b0f0ea7140bfd34e6742e81e43c3d1a96918
juanfu7467v <movistarcervise@gmail.com>
2025-12-06 15:56:31 -0500

    Delete handlers/messageHandler.js

--- handlers/messageHandler.js
+++
@@ -1,9 +0,0 @@
-import { workerData, parentPort } from "worker_threads";
-
-function processMessage({ message }) {
-  return `Hola 👋 recibí tu mensaje: ${message}`;
-}
-
-const response = processMessage(workerData);
-
-parentPort.postMessage(response);
